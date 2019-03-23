5e0f9e64ae2ba427c33b957d38b6dad43cb2770d
Shoraki, Mohsen <Mohsen.Shoraki1@T-Mobile.com>
2019-03-23 15:02:34 -0500

    import initial structure of project.

---
+++ src/main/java/com/galveston/eventvolunteers/EventVolunteersApplication.java
@@ -0,0 +1,13 @@
+package com.galveston.eventvolunteers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventVolunteersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventVolunteersApplication.class, args);
+    }
+
+}
